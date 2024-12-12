fea5bfa80e3d1d2b1e9c47759ceb0349c7df5b63
SamWarden <SamWardenSad@gmail.com>
2024-12-12 14:24:05 +0300

    refactory(tf): format the output file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "webserver_docker_containers" {
   description = "The created docker containers with webservers"
-  value = module.webserver.*.webserver_docker_container
+  value       = module.webserver.*.webserver_docker_container
 }
